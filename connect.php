4ba43cbbddc6ee9273c8413959871f9340de23f3
daviaquila <daviaquila@AQL.localdomain>
2020-02-15 02:57:05 -0200

    conexao do php com o banco

---
+++ connect.php
@@ -0,0 +1,19 @@
+
+<?php
+
+
+    $host = "localhost:3306";
+    $user = "integr64_coleta";
+    $pass = "123321";
+    $db = "integr64_COLETAWEB";
+
+  
+    //$host = "localhost";
+    //$user = "root";
+    //$pass = "";
+    //$db = "crud";
+    
+    $link = mysqli_connect($host, $user, $pass, $db);
+   
+
+?>
